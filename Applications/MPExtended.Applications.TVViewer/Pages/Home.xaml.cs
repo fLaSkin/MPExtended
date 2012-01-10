475e63b2987528658c365543ec2993a800244a54
TLindener <lindener.tobias@googlemail.com>
2012-01-10 16:33:33 +0100

    Added template for EPG view

--- Applications/MPExtended.Applications.TVViewer/Pages/Home.xaml.cs
+++ Applications/MPExtended.Applications.TVViewer/Pages/Home.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MPExtended.Services.TVAccessService.Interfaces;
+using System.Globalization;
 
 namespace MPExtended.Applications.TVViewer.Pages
 {
@@ -20,16 +22,73 @@ namespace MPExtended.Applications.TVViewer.Pages
     public partial class Home : Page
     {
         private bool _isWorking = false;
- 
+
         public Home()
         {
             InitializeComponent();
+            getListBoxData();
         }
 
         public void getListBoxData()
         {
-         
-        
+
+            List<WebChannelExtended> channelList = new List<WebChannelExtended>();
+            List<WebProgramBasic> list = new List<WebProgramBasic>();
+            list.Add(new WebProgramBasic
+            {
+                Title = "GZSZ",
+                StartTime = DateTime.Now.AddHours(5),
+                EndTime = DateTime.Now.AddHours(10)
+            });
+            list.Add(new WebProgramBasic
+{
+    Title = "Blabla",
+    StartTime = DateTime.Now.AddHours(10),
+    EndTime = DateTime.Now.AddHours(15)
+});
+            list.Add(new WebProgramBasic
+{
+    Title = "GZSZ",
+    StartTime = DateTime.Now.AddHours(15),
+    EndTime = DateTime.Now.AddHours(20)
+});
+            channelList.Add(new WebChannelExtended
+            {
+                Name = "ZDF",
+                Programs = list
+
+
+
+            });
+
+            lbChannels.ItemsSource = channelList;
         }
+
     }
+    public class WebChannelExtended
+    {
+        public string Name { get; set; }
+        public List<WebProgramBasic> Programs { get; set; }
+
+
+    }
+
+    public class MyMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan time = ((DateTime)values[1]).Subtract((DateTime)values[0]);
+            int percentage = 24 * time.Hours / 100;
+            int windowWidth = 600;
+            int width = windowWidth * percentage / 100;
+            return width;
+        }
+
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+    
+  
 }
